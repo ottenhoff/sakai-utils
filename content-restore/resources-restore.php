e68fe7d3d70e22c09c2f08f0e1098f7789bb0b08
Sam Ottenhoff <ottenhoff@longsight.com>
2014-01-06 11:51:31 -0500

    Allow restore in one step

--- content-restore/resources-restore.php
+++ content-restore/resources-restore.php
@@ -10,11 +10,50 @@ $date = $argv[3];
 $dir = $argv[4];
 $export = $argv[5];
 
-$mysqli = new mysqli('localhost', 'root', '', $db);
+$mysqli = new mysqli('localhost', 'root', '', 'aaa_archive');
 
 if (!$mysqli) {
     exit ("Bad database connection. \n");
 }
+
+// First need to restore the database
+$filename = "/mnt/massive06/b/mysql/server105-individual-increments/$db/$db.CONTENT_RESOURCE-schema.sql";
+if (!is_file($filename)) {
+  $filename = "/mnt/massive06/b/mysql/server106-individual-increments/$db/$db.CONTENT_RESOURCE-schema.sql";
+
+  if (!is_file($filename)) exit ("Could not find database file");
+}
+
+// Modify the schema
+$schema = explode("\n", file_get_contents($filename));
+$sql = '';
+foreach ($schema AS $line) {
+  if (strpos($line, " KEY ") !== FALSE) continue;
+  if (strpos($line, " SET ") !== FALSE) continue;
+  if (strpos($line, "BINARY") !== FALSE) {
+    $sql .= '  `BINARY_ENTITY` tinyint(1)';
+  }
+  elseif (strpos($line, "ENGINE") !== FALSE) {
+    $sql .= ') ENGINE=MyISAM DEFAULT CHARSET=utf8;';
+  }
+  else {
+    $sql .= $line . "\n";
+  }
+}
+
+// Drop the existing table if it exists
+$ret = $mysqli->query("DROP TABLE IF EXISTS CONTENT_RESOURCE");
+$ret = $mysqli->query($sql) or die(mysqli_error($mysqli));
+
+// Restore the CONTENT_RESOURCE TABLE
+$filename = str_replace("-schema", "", $filename);
+print "Starting rdiff restore of $filename \n";
+exec ("rdiff-backup -r \"$date\" $filename /tmp/cr-$db.sql ");
+print "Loading restored $filename into aaa_archive db \n";
+exec ("mysql -u root aaa_archive < /tmp/cr-$db.sql");
+$res = $mysqli->query("SELECT count(*) AS cnt FROM CONTENT_RESOURCE");
+$obj = $res->fetch_object();
+print "Restored $obj->cnt rows into CONTENT_RESOURCE table \n";
 
 $search = $mysqli->real_escape_string ($search);
 $res = $mysqli->query("SELECT * FROM CONTENT_RESOURCE WHERE RESOURCE_ID LIKE '%$search%' ");
@@ -43,7 +82,11 @@ while ($row = $res->fetch_object()) {
       if (!$ret) exit ("Incorrect permissions");
   }
 
-  if (!is_file ($file)) {
+  if (is_file ($dest)) {
+      echo ("File $dest already exists \n");
+      continue;
+  }
+  elseif (!is_file ($file)) {
       echo ("File missing attempting to retrieve: rdiff-backup -r \"$date\" $file \"$dest\" \n");
       exec ("rdiff-backup -r \"$date\" $file \"$dest\" ");
       sleep (2);
